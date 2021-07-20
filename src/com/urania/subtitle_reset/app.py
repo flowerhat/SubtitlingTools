3cc14340b91786a6233cbd55a852c8cb57382810
flowerhat <2322514676@qq.com>
2021-07-20 19:09:50 +0800

    update: Optimize performance and simplify output

--- src/com/urania/subtitle_reset/app.py
+++ src/com/urania/subtitle_reset/app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# This is a english subtitle reset application based on the integrity of sentence.
-# It can output the reset text on the console that provides convenience for calling the translation API next.
+# This is an english subtitle reset application based on the integrity of sentence.
+# It returns a generator that provides convenience for calling the translation API next.
 # The text reset work depends on ntlk.
 # Each duration will be reset by a algorithm according to the proportion of characters.
 
@@ -22,7 +22,9 @@
 #   00:00:07.388 --> 00:00:10.436
 #   So dynamic programming is one of my most favorite topics to teach.
 
+from collections import deque
 import datetime
+import codecs
 import pysrt
 import nltk
 
@@ -44,56 +46,45 @@ class Util:
 
 
 def app(path):
-    # Load srt
-    srt: list[: pysrt.SubRipItem] = pysrt.open(path=path)
+    with codecs.open(path, encoding='utf-8') as file:
+        source = deque(dict(
+            start_time=Util.to_datetime(sub.start),
+            total_seconds=Util.to_timedelta(sub.duration).total_seconds(),
+            text=sub.text.replace('\n', ' ') + ' '
+        ) for sub in pysrt.stream(file))
+
+    text = ''.join([item['text'] for item in source])
 
     # Load tokenizer
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-    # Prepare to tokenize.
-    # It's necessary to replace but will cause one more space than each original paragraph
-    all_plain_text = srt.text.replace("\n", " ")
-
     # Tokenize
-    text_lst: list = tokenizer.tokenize(all_plain_text)
-
-    # Align to all_plain_text
-    for item in srt:
-        item.text = item.text + " "
+    sentences = deque(tokenizer.sentences_from_text(text))
 
     # Set all indexes of end character as objs
-    objs: list[: int] = [tup[1] for tup in tokenizer.span_tokenize(all_plain_text)]
+    objs = [tup[1] for tup in tokenizer.span_tokenize(text)]
 
-    # Set start of duration as lp:left pointer
-    lp = Util.to_datetime(srt[0].start)
-
-    # Initialize scanned area
+    # Initialize parameter for scan
+    item: dict = source.popleft()
     start = 0
-    iter_srt: iter = iter(srt)
-    item: pysrt.SubRipItem = next(iter_srt)
-    step = len(item.text)
-
-    # Initialize buffer
-    string_buffer = ""
+    step = len(item['text'])
+    lp = item['start_time']
 
     # Scan and reset
     for obj in objs:
         while not start <= obj <= (start + step):
             start += step
-            item: pysrt.SubRipItem = next(iter_srt)
-            step = len(item.text)
+            item = source.popleft()
+            step = len(item['text'])
         else:
-            # Set the combination of (item.start) and (new duration) as rp:right pointer
-            rp = Util.to_datetime(item.start) + datetime.timedelta(seconds=((obj - start + 1) / step) * Util.to_timedelta(item.duration).total_seconds())
+            # Set the combination of (item.start) and (new total_seconds) as rp:right pointer
+            rp = item['start_time'] + datetime.timedelta(seconds=((obj - start + 1) / step) * item['total_seconds'])
 
-            # Update buffer
-            string_buffer += ''.join([str(lp)[11:23], ' ', item.TIMESTAMP_SEPARATOR, ' ', str(rp)[11:23], '\n' + text_lst.pop(0) + '\n\n'])
+            # Append new item
+            yield ''.join([str(lp)[11:23], ' --> ', str(rp)[11:23]]), sentences.popleft()
 
             # Update lp of duration
             lp = rp
-
-    # Output
-    print(string_buffer)
 
 
 if __name__ == '__main__':
